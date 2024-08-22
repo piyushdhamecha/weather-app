eb92627a9dd6376839d69863f4b340fe84c6fb15
Piyush <piyush.dhamecha@gmail.com>
2024-08-22 18:47:08 +0530

    init

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import React from "react";
+import ThemeDropdown from "./ThemeDropdown/ThemeDropdown";
+import { Github } from "lucide-react";
+
+const Navbar = () => {
+  const router = useRouter();
+
+  return (
+    <div className="w-full py-4 flex item-center justify-between">
+      <div className="left"></div>
+      <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <ThemeDropdown />
+        <Button
+          className="source-code flex item-center gap-2"
+          onClick={() => router.push("https://github.com")}
+        >
+          <Github />
+          Source Code
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/utils/icons.tsx
@@ -0,0 +1,4 @@
+import { Github, Sun } from "lucide-react";
+
+export const github = <Github size={15} />;
+export const sub = <Sun size={15} />;
