cd4166e15cd538f213bf1168e2a837b70a4ffc3e
Piyush <piyush.dhamecha@gmail.com>
2024-08-27 14:44:46 +0530

    Air pollution component

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
         <div className="flex flex-col w-full">
           <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
             <AirPollution />
-            <AirPollution />
           </div>
         </div>
       </div>
